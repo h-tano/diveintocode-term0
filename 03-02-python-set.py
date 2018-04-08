182a3f35c613a97a97b4a69bdb8c6796006ad318
suzuki kazufumi <skazufumi.a02525@gmail.com>
2018-04-08 14:35:53 +0900

    入力値判定を追加

--- 03-02-python-set.py
+++ 03-02-python-set.py
@@ -7,26 +7,32 @@ course_dict = {
 
 
 def find_person(want_to_find_person):
-    #辞書のキーを取得する
-    dic_list = list(course_dict.keys())
-    #各コースで在籍判定および結果の表示を行う
-    for i in dic_list:
-        #コースに探したい人が在籍しているか積を取って確認する
-        result = course_dict[i] & want_to_find_person
-        ##探したい人が何人在籍しているかを調べる
+    # 各コースで在籍判定および結果の表示を行う
+    if type(want_to_find_person) == set:
+        want_to_find_person = set(want_to_find_person)
+    else:
+        print("入力をset形式にしてください。")
+        print("例：{'Aさん','Bさん'}")
+        return None
+
+    for course_name in course_dict.keys():
+        # コースに探したい人が在籍しているか積を取って確認する
+        result = course_dict[course_name] & want_to_find_person
+        # 探したい人が何人在籍しているかを調べる
         number = len(result)
-        #探したい人の在籍人数が０人なら"〇〇さんは在籍していません"と表示する
+        # 探したい人の在籍人数が０人なら"〇〇さんは在籍していません"と表示する
         if number == 0:
-            print("{}に{}は在籍していません。".format(i,want_to_find_person))
-        #探したい人の在籍人数が１人なら"〇〇さんのみ在籍しています"と表示する
-        elif number == 1:
-            print("{}に{}のみ在籍しています。".format(i,result))
-        ##探したい人の在籍人数が2人以上なら"〇〇さんは在籍しています"と表示する
-        else :
-            print("{}に{}は在籍しています。".format(i,result))
+            print("{}に{}は在籍していません。".format(course_name, want_to_find_person))
+        # 探したい人の在籍人数が１人かつ探したい人の人数が複数名なら"〇〇さんのみ在籍しています"と表示する
+        elif number == 1 and len(want_to_find_person) > 1:
+            print("{}に{}のみ在籍しています。".format(course_name, result))
+        # 探したい人の在籍人数が2人以上なら"〇〇さんは在籍しています"と表示する
+        else:
+            print("{}に{}は在籍しています。".format(course_name, result))
+
 
 def main():
-    want_to_find_person = {'Cさん', 'Aさん'}
+    want_to_find_person = ['Cさん', 'Aさん']
     print('探したい人: {}'.format(want_to_find_person))
     find_person(want_to_find_person)
 
